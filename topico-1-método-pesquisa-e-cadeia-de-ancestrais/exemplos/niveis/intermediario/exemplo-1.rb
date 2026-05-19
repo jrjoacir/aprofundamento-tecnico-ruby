3a6f6cfa570e2c4c1ba99f26bc250acf01606d2b
Joacir Júnior <jr.joacir@gmail.com>
2026-05-19 12:27:37 -0300

    Cria example-1.rb para exibição ancestrais de nível intermediário
    Mostra ancestrais adicionando módulos com métodos include e prepend na classe User.

---
+++ topico-1-método-pesquisa-e-cadeia-de-ancestrais/exemplos/niveis/intermediario/exemplo-1.rb
@@ -0,0 +1,28 @@
+# Nível: Intermediário
+# Diferença prática na ordem de execução entre include e prepend.
+  
+module Loggable
+  def greet
+    "Log: Calling greet -> " + super
+  end
+end
+
+module Authenticated
+  def greet
+    "Auth: Checking permissions -> " + super
+  end
+end
+
+class User
+  include Loggable
+  prepend Authenticated
+
+  def greet
+    "Hello User!"
+  end
+end
+
+# Cadeia de Ancestrais de User: [Authenticated, User, Loggable, Object...]
+puts User.ancestors.inspect
+puts User.new.greet 
+# => "Auth: Checking permissions -> Log: Calling greet -> Hello User!"
